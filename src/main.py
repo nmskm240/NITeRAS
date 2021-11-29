6566efe714bc74e6fa865f9a839aa047d1643e0b
nmskm240 <bokaronogod@gmail.com>
2021-11-29 14:05:26 +0900

    Support Linux

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
 import tkinter as tk
+import platform
 
 from dotenv import load_dotenv
+load_dotenv()
 
 from scene_management.scenes.home import Home
 from scene_management.scene_manager import SceneManager
 
-load_dotenv()
-
 app = tk.Tk()
-app.state("zoom")
+if platform.system() == "Windows":
+    app.state("zoom")
+else:
+    app.attributes("-zoomed", True)
 app.resizable(False, False)
 app.title("NITeRAS")
 
