9702dce2aa64e990c4242545dc8065066d0dae38
nmskm240 <bokaronogod@gmail.com>
2021-12-20 20:07:08 +0900

    play error and success sound

--- src/scene_management/scenes/error.py
+++ src/scene_management/scenes/error.py
@@ -1,6 +1,11 @@
+from playsound import playsound
 from scene_management.scenes.announce import Announce
 
 class Error(Announce):
     def __init__(self, master, body: str):
         super().__init__(master, "ERROR", body)
         self.title.config(fg="red")
+
+    def on_load(self) -> None:
+        playsound("sounds/error.mp3")
+        return super().on_load()
--- src/scene_management/scenes/success.py
+++ src/scene_management/scenes/success.py
@@ -1,8 +1,11 @@
-import tkinter as tk
-
+from playsound import playsound
 from scene_management.scenes.announce import Announce
 
 class Success(Announce):
     def __init__(self, master, body: str):
         super().__init__(master, "SUCCESS", body)
         self.title.config(fg="green")
+
+    def on_load(self) -> None:
+        playsound("sounds/success.mp3")
+        return super().on_load()
